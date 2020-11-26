93f99863558004ae6de9c8d2961db3420130af14
itlgcml <1551210419@qq.com>
2020-11-26 09:46:54 +0800

    修改配置文件

--- springboot-dubbo-demo/springboot-dubbo-provider/src/main/java/com/itlg/mapper/DemoMapper.java
+++ springboot-dubbo-demo/springboot-dubbo-provider/src/main/java/com/itlg/mapper/DemoMapper.java
@@ -2,9 +2,12 @@ package com.itlg.mapper;
 
 
 
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.itlg.entity.bo.Message;
+
 import java.util.List;
 
-public interface DemoMapper {
+public interface DemoMapper extends BaseMapper {
 
     public List<Message> findMessage();
 
