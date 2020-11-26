065b5d251e6c11c4806e228e2c2c7c6e408ffbfa
itlgcml <1551210419@qq.com>
2020-11-26 09:45:44 +0800

    mybatis修改为mybatisplus

--- springboot-dubbo-demo/springboot-dubbo-api/src/main/java/com/itlg/service/DemoService.java
+++ springboot-dubbo-demo/springboot-dubbo-api/src/main/java/com/itlg/service/DemoService.java
@@ -1,7 +1,5 @@
 package com.itlg.service;
 
-import com.itlg.entity.bo.Message;
-
 import java.util.List;
 
 public interface DemoService {
--- springboot-dubbo-demo/springboot-dubbo-provider/src/main/java/com/itlg/mapper/DemoMapper.java
+++ springboot-dubbo-demo/springboot-dubbo-provider/src/main/java/com/itlg/mapper/DemoMapper.java
@@ -2,8 +2,6 @@ package com.itlg.mapper;
 
 
 
-import com.itlg.entity.bo.Message;
-
 import java.util.List;
 
 public interface DemoMapper {
