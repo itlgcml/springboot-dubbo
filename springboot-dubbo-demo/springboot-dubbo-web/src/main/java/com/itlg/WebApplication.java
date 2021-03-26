f7521f86de0a0552a3d2db3bcba9510ca9981fa6
itlgcml <1551210419@qq.com>
2021-03-26 15:35:55 +0800

    测试错误提交

--- springboot-dubbo-demo/springboot-dubbo-web/src/main/java/com/itlg/WebApplication.java
+++ springboot-dubbo-demo/springboot-dubbo-web/src/main/java/com/itlg/WebApplication.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @ImportResource("classpath:*.xml")
+//idea暂存代码
 public class WebApplication {
 
     public static void main(String[] args) {
