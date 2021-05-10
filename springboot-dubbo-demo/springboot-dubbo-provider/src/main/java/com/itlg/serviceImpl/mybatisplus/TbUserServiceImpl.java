75a4213c2a4f3040b3a19c27f37611b457d972db
itlgcml <1551210419@qq.com>
2021-05-10 10:40:50 +0800

    取消xxxjob连接超时一直打印日志，获取web项目绝对路径的两种方式

--- springboot-dubbo-demo/springboot-dubbo-provider/src/main/java/com/itlg/serviceImpl/mybatisplus/TbUserServiceImpl.java
+++ springboot-dubbo-demo/springboot-dubbo-provider/src/main/java/com/itlg/serviceImpl/mybatisplus/TbUserServiceImpl.java
@@ -13,6 +13,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.ServletContext;
 import java.util.List;
 
 @Service(value = "tbUserService")
--- springboot-dubbo-demo/springboot-dubbo-web/src/main/java/com/itlg/controller/mybatisplus/TbUserController.java
+++ springboot-dubbo-demo/springboot-dubbo-web/src/main/java/com/itlg/controller/mybatisplus/TbUserController.java
@@ -1,5 +1,6 @@
 package com.itlg.controller.mybatisplus;
 
+import com.itlg.config.SpringBeanTool;
 import com.itlg.entity.bo.mybatisplus.TbUserBO;
 import com.itlg.entity.bo.mybatisplus.UserOrderBO;
 import com.itlg.excepion.R;
@@ -11,7 +12,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.ContextLoader;
 
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServlet;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -24,6 +30,9 @@ public class TbUserController {
 
     @Autowired
     private UserOrderBOService userOrderBOService;
+
+    @Autowired
+    private SpringBeanTool springBeanTool;
 
     @GetMapping(value = "/all")
     @ApiOperation(value = "查询所有用户")
@@ -41,5 +50,18 @@ public class TbUserController {
     @ApiOperation(value = "mybatis一对多-查询所有用户以及其下订单")
     public R<List<UserOrderBO>> allUserOrder() {
         return userOrderBOService.allUserOrder();
+    }
+
+    @GetMapping(value = "/address")
+    @ApiOperation(value = "项目中获取路径的两种方式")
+    public R<List<String>> address() {
+        ServletContext servletContext = springBeanTool.getServletContext();
+        URL url = TbUserController.class.getClassLoader().getResource("");
+        String realPath = servletContext.getRealPath("");
+        String file = url.getFile();
+        List<String> list = new ArrayList<>();
+        list.add(realPath);
+        list.add(file);
+        return R.success(list);
     }
 }
